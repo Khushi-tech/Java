75cd4a16bd9c22f6adeb8e638b7e3e6d64dd8b2a
Khushi <khushiupadhyay0999@gmail.com>
2026-01-27 09:38:48 +0530

    Intial commit

---
+++ Lab_001.java
@@ -0,0 +1,10 @@
+package programs;
+
+public class Lab_001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Hello world, This is ATB13X");
+	}
+
+}
