b07111aebf1a56e70c9ea159da831eee304e08af
Khushi <khushiupadhyay0999@gmail.com>
2026-02-22 22:34:31 +0530

    Lab002_Variable_declaration_example

---
+++ Lab002.java
@@ -0,0 +1,11 @@
+package programs;
+
+public class Lab002 {
+	public static void main(String args[]) {
+		int number =10;
+		String name = "Khushi";
+		System.out.println(number);	
+		System.out.println(name);	
+	}
+
+}
