4dadecdd774857ab4794c8b401501917da556bbd
Khushi <khushiupadhyay0999@gmail.com>
2026-02-22 22:45:45 +0530

    Adding more variable

--- Lab002.java
+++ Lab002.java
@@ -2,10 +2,29 @@ package programs;
 
 public class Lab002 {
 	public static void main(String args[]) {
-		int number =10;
-		String name = "Khushi";
-		System.out.println(number);	
-		System.out.println(name);	
+
+	        // Integer variable
+	        int age = 25;
+
+	        // Double variable
+	        double salary = 45000.50;
+
+	        // Character variable
+	        char grade = 'A';
+
+	        // Boolean variable
+	        boolean isAutomationTester = true;
+
+	        // String variable
+	        String name = "Khushi";
+
+	        // Printing variables
+	        System.out.println("Name: " + name);
+	        System.out.println("Age: " + age);
+	        System.out.println("Salary: " + salary);
+	        System.out.println("Grade: " + grade);
+	        System.out.println("Is Automation Tester: " + isAutomationTester);
+	    
 	}
 
 }
