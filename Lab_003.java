0861df59234c5aecc722187f885eeba5b5193330
Khushi <khushiupadhyay0999@gmail.com>
2026-02-27 13:29:37 +0530

    Adding final variable

---
+++ Lab_003.java
@@ -0,0 +1,8 @@
+package programs;
+
+public class Lab_003 {
+	public static void main(String args[]) {
+	final int MINUTES_PER_HOUR = 60;
+	System.out.println("The variable value is "+ MINUTES_PER_HOUR);
+}
+}
