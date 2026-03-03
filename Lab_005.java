361662246bc66fe32a32a97571ae8f0698acd237
Khushi <khushiupadhyay0999@gmail.com>
2026-03-03 10:12:30 +0530

    Adding program demonstrating if else program

---
+++ Lab_005.java
@@ -0,0 +1,19 @@
+package programs;
+import java.util.Scanner;
+
+public class Lab_005 {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter a number: ");
+	        int number = sc.nextInt();
+
+	        if (number % 2 == 0) {
+	            System.out.println("Even Number");
+	        } else {
+	            System.out.println("Odd Number");
+	        }
+
+	        sc.close();
+	    }
+
+}
