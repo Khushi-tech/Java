b25bba3d05008d04006aafbba2acb3d733b1dc18
Khushi <khushiupadhyay0999@gmail.com>
2026-03-05 23:48:06 +0530

    Adding nested if else if

---
+++ Lab_006.java
@@ -0,0 +1,27 @@
+package programs;
+
+public class Lab_006{
+
+    public static void main(String[] args) {
+
+        int number = 10;
+
+        if (number > 0) {
+            System.out.println("The number is positive");
+        } else if (number < 0) {
+            System.out.println("The number is negative");
+        } else {
+            System.out.println("The number is zero");
+        }
+
+        // Another example
+        int age = 18;
+
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+
+    }
+}
